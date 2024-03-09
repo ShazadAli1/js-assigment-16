0e3e9ede7765be4f3f88644a052e2911a0543289
Shazad Ali <155179705+ShazadAli1@users.noreply.github.com>
2024-03-09 13:00:00 +0500

    Add files via upload

---
+++ javascript assigment/app.js
@@ -0,0 +1,42 @@
+// alert ( "Please! Enter a valid password" );
+
+// alert ( "Welcome to JS Land... \n Happy Coding!" );
+
+//  alert ( "Welcome to JS land..." );
+
+//  alert ( "Happy Coding! \n" );
+
+//  alert ("Hello I can run JS through my web browser's Console" );
+//  console.log (alert);
+
+
+
+// var userName1;
+
+// alert(  "Shazad Ali" );
+ 
+// console.log  (userName1);
+
+
+// var userName2 ;
+
+// alert(  "23 years old" );
+ 
+// console.log  (userName2);
+
+
+// var userName3 ;
+
+// alert(  "Certified Mobile Application Development" );
+ 
+// console.log  (userName3);
+
+//  var userName;
+//  alert ( "Pizza \n Pizz \n Piz \n Pi\n P"  )
+
+  var userName;
+
+  alert("My Email address is example@example.com");
+
+
+
